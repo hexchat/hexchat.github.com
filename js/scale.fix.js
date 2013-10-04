47f2fdde841738ca9ea856b15e3c5d02153e916c
XhmikosR <xhmikosr@users.sourceforge.net>
2013-10-04 06:39:57 +0300

    Fix JSHint warnings.

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -1,10 +1,10 @@
 fixScale = function(doc) {
 
 	var addEvent = 'addEventListener',
-	    type = 'gesturestart',
-	    qsa = 'querySelectorAll',
-	    scales = [1, 1],
-	    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+		type = 'gesturestart',
+		qsa = 'querySelectorAll',
+		scales = [1, 1],
+		meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
 
 	function fix() {
 		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
@@ -13,7 +13,7 @@ fixScale = function(doc) {
 
 	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
 		fix();
-		scales = [.25, 1.6];
+		scales = [0.25, 1.6];
 		doc[addEvent](type, fix, true);
 	}
 
